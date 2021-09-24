c955708aa39df2c56a233619a833a1d01a0afb12
Fredrik Strömbäck <fredde.stromback@gmail.com>
2021-09-24 04:25:49 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <p>{{ greeting }}</p>
+  <input type="text" v-model="greeting" />
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+  data: () => {
+    return {
+      greeting: 'Hello World!'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+[v-cloak] {
+  display: none;
+}
+</style>
